77bf42f257a3a07c622c6630a9aa6d89c5a8fc27
Anders Bjerner <abjerner@skybrud.dk>
2020-04-30 14:42:43 +0200

    Build stuff

--- release.bat
+++ release.bat
@@ -1,5 +1,2 @@
 @echo off
-cd src/Skybrud.Social.Vimeo
-"C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin\msbuild.exe" /t:pack /p:Configuration=Release /p:PackageOutputPath=../../releases/nuget /p:RunBuildTargets=false
-cd ../../
-grunt
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin\msbuild.exe" src/Skybrud.Social.Vimeo/Skybrud.Social.Vimeo.csproj /t:rebuild /t:pack /p:Configuration=Release /p:BuildTools=1 /p:PackageOutputPath=../../releases/nuget
--- src/Skybrud.Social.Vimeo/Properties/AssemblyInfoVersion.cs
+++
@@ -1,3 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyMetadata("StartDate", "2013-09-22")]
