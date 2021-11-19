e367bcb97d901633aef533d70f2793ef12ec7f2a
Anders Bjerner <abjerner@skybrud.dk>
2021-11-19 13:44:29 +0100

    Some code adjustments following Jesper's PR
    + code formatting
    + introduced enum type for the video quality
    + video file created time should be represented by EssentialsTime rather than a string
    + "FPS" property name should be "Fps" per .NET naming conventions
    + changed "Size" property from int to long (if video file sizes can be greater than 2 GB 🤔)
    + improved the XML documentation a bit

--- src/Skybrud.Social.Vimeo/Models/Videos/VimeoVideoFile.cs
+++ src/Skybrud.Social.Vimeo/Models/Videos/VimeoVideoFile.cs
@@ -1,85 +1,114 @@
 ﻿using Newtonsoft.Json.Linq;
 using Skybrud.Essentials.Json.Extensions;
+using Skybrud.Essentials.Time;
 
-namespace Skybrud.Social.Vimeo.Models.Videos
-{
+namespace Skybrud.Social.Vimeo.Models.Videos {
+    
     /// <summary>
     /// Class describing a file of a Vimeo video.
     /// </summary>
-    public class VimeoVideoFile : VimeoObject
-    {
+    public class VimeoVideoFile : VimeoObject {
+
+        #region Properties
+        
         /// <summary>
-        /// Gets the Video quality - hd, mobile, sd, hls
+        /// Gets the video quality.
         /// </summary>
-        public string Quality { get; }
+        public VimeoVideoQuality Quality { get; }
+        
         /// <summary>
-        /// Video MIME type, fx video/mp4
+        /// Gets the MIME type of the video file - eg. <c>video/mp4</c>.
         /// </summary>
         public string Type { get; }
+        
         /// <summary>
-        /// Video width in pixels
+        /// Gets the width in pixels.
         /// </summary>
         public int Width { get; }
+        
         /// <summary>
-        /// Video height in pixels
+        /// Gets the height in pixels.
         /// </summary>
         public int Height { get; }
+        
         /// <summary>
-        /// The Vimeo src link including oauth token that can be used in HTML5 video elements.
+        /// Gets the Vimeo src link including OAuth token that can be used in HTML5 video elements.
         /// </summary>
         public string Link { get; }
+        
         /// <summary>
-        /// Create time in the format YYYY-MM-DDTHH:mm:ssZ
+        /// Gets the timestamp for when the file was created.
         /// </summary>
-        public string CreatedTime { get; }
+        public EssentialsTime CreatedTime { get; }
+        
         /// <summary>
-        /// FPS of the file
+        /// Gets the frames per second of the video file.
         /// </summary>
-        public int FPS { get; }
+        public int Fps { get; }
+        
         /// <summary>
-        /// Size in bytes
+        /// Gets the video file's size in bytes.
         /// </summary>
-        public int Size { get; }
+        public long Size { get; }
+        
         /// <summary>
-        /// Md5 hash of the file
+        /// Gets the MD5 hash of the file.
         /// </summary>
         public string Md5 { get; }
+        
         /// <summary>
-        /// Public name of the quality of the file - fx HD 1080p
+        /// Gets the public name of the quality of the file - eg <c>HD 1080p</c>.
         /// </summary>
         public string PublicName { get; }
+        
         /// <summary>
-        /// Size in a more readable way, fx 32.18MB
+        /// Gets the video file's size in a more readable way - eg <c>32.18MB</c>.
         /// </summary>
         public string SizeShort { get; }
 
-        // Example obj:
-        //{
-        //    "quality": "hd",
-        //    "type": "video/mp4",
-        //    "width": 1918,
-        //    "height": 1080,
-        //    "link": "https://player.vimeo.com/external/123456789.hd.mp4?s=91e0428c9c38b28b275bdebe45939221f3fe6cf7&profile_id=119&oauth_token_id=1234567890",
-        //    "created_time": "2015-03-19T07:49:29+00:00",
-        //    "fps": 25,
-        //    "size": 69708426,
-        //    "md5": "91fca61b68a131868dc5d899862ae9eb",
-        //    "public_name": "HD 1080p",
-        //    "size_short": "66.48MB"
-        //}
-        private VimeoVideoFile(JObject obj) : base(obj)
-        {
-            Quality = obj.GetString("quality");
+        #endregion
+
+        #region Constructors
+
+        private VimeoVideoFile(JObject obj) : base(obj) {
+            Quality = obj.GetString("quality", ParseQuality);
             Type = obj.GetString("type");
             Width = obj.GetInt32("width");
             Height = obj.GetInt32("height");
             Link = obj.GetString("link");
-            CreatedTime = obj.GetString("created_time");
-            FPS = obj.GetInt32("fps");
-            Size = obj.GetInt32("size");
+            CreatedTime = obj.GetString("created_time", EssentialsTime.Parse);
+            Fps = obj.GetInt32("fps");
+            Size = obj.GetInt64("size");
             Md5 = obj.GetString("md5");
             PublicName = obj.GetString("public_name");
             SizeShort = obj.GetString("size_short");
+        }
+
+        #endregion
+
+        #region Static methods
+
+        private static VimeoVideoQuality ParseQuality(string value) {
+
+            switch (value) {
+                
+                case "sd":
+                    return VimeoVideoQuality.Standard;
+
+                case "hd":
+                    return VimeoVideoQuality.HighDefinition;
+
+                case "mobile":
+                    return VimeoVideoQuality.Mobile;
+
+                case "hls":
+                    return VimeoVideoQuality.Streaming;
+
+                default:
+                    return VimeoVideoQuality.Unrecognized;
+
+            }
+
         }
 
         /// <summary>
@@ -87,9 +116,12 @@ namespace Skybrud.Social.Vimeo.Models.Videos
         /// </summary>
         /// <param name="obj">The instance of <see cref="JObject"/> to be parsed.</param>
         /// <returns>An instance of <see cref="VimeoVideoFile"/>.</returns>
-        public static VimeoVideoFile Parse(JObject obj)
-        {
+        public static VimeoVideoFile Parse(JObject obj) {
             return obj == null ? null : new VimeoVideoFile(obj);
         }
+
+        #endregion
+
     }
+
 }
---
+++ src/Skybrud.Social.Vimeo/Models/Videos/VimeoVideoQuality.cs
@@ -0,0 +1,32 @@
+﻿namespace Skybrud.Social.Vimeo.Models.Videos {
+    
+    /// <summary>
+    /// Enum class representing the quality of a <see cref="VimeoVideoFile"/>.
+    /// </summary>
+    public enum VimeoVideoQuality {
+        
+        /// <summary>
+        /// Indicates that the file is of standard quality.
+        /// </summary>
+        Standard,
+        
+        /// <summary>
+        /// Indicates that the video file is of high definition quality.
+        /// </summary>
+        HighDefinition,
+        
+        /// <summary>
+        /// Indicates that the video file quality is suited for mobile devices.
+        /// </summary>
+        Mobile,
+        
+        /// <summary>
+        /// Indicates that the video file quality is suited for mobile devices.
+        /// </summary>
+        Streaming,
+        
+        Unrecognized
+
+    }
+
+}
