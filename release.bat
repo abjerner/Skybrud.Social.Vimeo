2dbe8f66e3481e6ff1c7a041282739e1cbac0c0e
Anders Bjerner <abjerner@skybrud.dk>
2021-11-19 14:25:07 +0100

    Use dotnet build instead of msbuild 😎

--- release.bat
+++ release.bat
@@ -1,2 +1,2 @@
 @echo off
-"C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin\msbuild.exe" src/Skybrud.Social.Vimeo/Skybrud.Social.Vimeo.csproj /t:rebuild /t:pack /p:Configuration=Release /p:BuildTools=1 /p:PackageOutputPath=../../releases/nuget
+dotnet build src/Skybrud.Social.Vimeo --configuration Release /t:rebuild /t:pack -p:BuildTools=1 -p:PackageOutputPath=../../releases/nuget
