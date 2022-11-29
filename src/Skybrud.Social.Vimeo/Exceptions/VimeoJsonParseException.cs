5c35bb166bd3d9e8d34d89b93f3e81a9ded5b9d2
Anders Bjerner <abjerner@skybrud.dk>
2022-11-29 13:21:54 +0100

    Exception class shouldn't expose the JSON directly via the exception message

--- src/Skybrud.Social.Vimeo/Exceptions/VimeoJsonParseException.cs
+++ src/Skybrud.Social.Vimeo/Exceptions/VimeoJsonParseException.cs
@@ -32,7 +32,7 @@ namespace Skybrud.Social.Vimeo.Exceptions {
         /// </summary>
         /// <param name="json">The JSON token that resulted in the error.</param>
         /// <param name="innerException">The exception that is the cause of the current exception, or a <see langword="null"/> reference if no inner exception is specified.</param>
-        public VimeoJsonParseException(JToken json, Exception? innerException) : base("Failed parsing JSON object.\r\n\r\n" + json, innerException!) {
+        public VimeoJsonParseException(JToken json, Exception? innerException) : base("Failed parsing JSON object.", innerException!) {
             Json = json;
         }
 
