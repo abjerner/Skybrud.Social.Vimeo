1bcd64230756c4e8f7fd6a89f7e311bf23d04d5c
Anders Bjerner <abjerner@skybrud.dk>
2023-05-04 00:23:26 +0200

    Added missing values to the "VimeoAccountType" enum

--- src/Skybrud.Social.Vimeo/Models/Users/VimeoAccountType.cs
+++ src/Skybrud.Social.Vimeo/Models/Users/VimeoAccountType.cs
@@ -3,25 +3,58 @@
     /// <summary>
     /// Indicates the account type of a Vimeo user.
     /// </summary>
+    /// <see>
+    ///     <cref>https://developer.vimeo.com/api/reference/response/user</cref>
+    /// </see>
     public enum VimeoAccountType {
 
         /// <summary>
-        /// Indicates a user with a basic membership (free).
+        /// Indicates that a user has a <strong>Vimeo Advanced</strong> subscription.
+        /// </summary>
+        Advanced,
+
+        /// <summary>
+        /// Indicates that a user has a <strong>Vimeo Basic</strong> subscription..
         /// </summary>
         Basic,
 
         /// <summary>
-        /// Indicates a user with a <strong>Business</strong> membership.
+        /// Indicates a user has a <strong>Vimeo Business</strong> subscription.
         /// </summary>
         Business,
 
         /// <summary>
-        /// Indicates a user with a <strong>Plus</strong> membership.
+        /// Indicates a user has a <strong>Vimeo Enterprise</strong> subscription.
+        /// </summary>
+        Enterprise,
+
+        /// <summary>
+        /// Indicates a user has a <strong>Vimeo Free</strong> subscription.
+        /// </summary>
+        Free,
+
+        /// <summary>
+        /// Indicates a user has a <strong>Vimeo Business Live</strong> subscription.
+        /// </summary>
+        LiveBusiness,
+
+        /// <summary>
+        /// Indicates a user has a <strong>Vimeo Premium</strong> subscription.
+        /// </summary>
+        LivePremium,
+
+        /// <summary>
+        /// Indicates a user has a <strong>Vimeo PRO Live</strong> subscription.
+        /// </summary>
+        LivePro,
+
+        /// <summary>
+        /// Indicates a user with a <strong>Vimeo Plus</strong> membership.
         /// </summary>
         Plus,
 
         /// <summary>
-        /// Indicates a user with a <strong>Pro</strong> membership.
+        /// Indicates a user with a <strong>Vimeo Pro</strong> membership.
         /// </summary>
         Pro,
 
@@ -31,7 +64,22 @@
         /// <see>
         ///     <cref>https://join.vimeo.com/pro-unlimited/</cref>
         /// </see>
-        ProUnlimited
+        ProUnlimited,
+
+        /// <summary>
+        /// Indicates a user with a <strong>Vimeo Producer</strong> membership.
+        /// </summary>
+        Producer,
+
+        /// <summary>
+        /// Indicates a user with a <strong>Vimeo Standard</strong> membership.
+        /// </summary>
+        Standard,
+
+        /// <summary>
+        /// Indicates a user with a <strong>Vimeo Starter</strong> membership.
+        /// </summary>
+        Starter
 
     }
 
